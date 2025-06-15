c17a3e399e575c691a35d85ab0a65dccae64f84c
Kieran Wright <kieran.jwright@hotmail.co.uk>
2025-06-15 22:36:11 +0100

    Remove "Play Loop / Oneshot" menu item

--- app.py
+++ app.py
@@ -6,96 +6,7 @@ import pyaudio
 import selectors
 import urwid
 
-def null_callback(in_data,frame_count,time_info,status):
-    return (b'x00',pyaudio.paAbort) # End audio stream.
-
 # Globals
-continue_playing_audio = False
-nr_audio_callbacks_run = 0
-stop_audio_loopback = True
-pa = pyaudio.PyAudio()
-stream = pa.open(format=pa.get_format_from_width(2,True),
-                channels=1,
-                rate=44100,
-                output=True,
-                stream_callback=null_callback)
-stream.stop_stream()
-stream.close()
-octave = 1
-piano_key_nr = 0
-slope = 0.0
-
-def play_callback(in_data,frame_count,time_info,status):
-    global nr_audio_callbacks_run
-    global stop_audio_loopback
-
-    sample = -1.0
-    data = list()
-    if nr_audio_callbacks_run > 50:
-        if stop_audio_loopback:
-            return (b'x00',pyaudio.paAbort) # End audio stream.
-        else:
-            nr_audio_callbacks_run = 0
-    for frame_index in range(frame_count * 2):
-        sample_word = int((sample + 1.0) / 2.0 * 65535)
-        data.append(int(sample_word / 255) % 255)
-        data.append(int(sample_word % 255))
-        next_sample = sample + 0.1 / (nr_audio_callbacks_run + 1)
-        sample = next_sample if next_sample < 1.0 else -1.0
-    nr_audio_callbacks_run += 1
-    return (bytes(data),pyaudio.paContinue)
-
-async def stream_audio():
-    global stop_audio_loopback
-    global stream
-
-    stream.stop_stream()
-    stream.start_stream()
-
-    while stream.is_active():
-        await asyncio.sleep(0)
-    
-    if stop_audio_loopback:
-        stream.stop_stream()
-
-def play_once(loop):
-    global nr_audio_callbacks_run
-    global stop_audio_loopback
-
-    nr_audio_callbacks_run = 0
-    stop_audio_loopback = True
-
-    asyncio.create_task(stream_audio())
-
-def play_continuously(loop):
-    global nr_audio_callbacks_run
-    global stop_audio_loopback
-    
-    nr_audio_callbacks_run = 0
-    stop_audio_loopback = False
-
-    asyncio.create_task(stream_audio())
-
-def stop_playing(loop):
-    global stop_audio_loopback
-    global stream
-
-    stop_audio_loopback = True
-    stream.stop_stream()
-
-def play_activate(loop):
-    global stream
-
-    stream = pa.open(format=pa.get_format_from_width(2,True),
-                    channels=1,
-                    rate=44100,
-                    output=True,
-                    stream_callback=play_callback)
-    stream.stop_stream()
-
-def play_deactivate(loop):
-    stream.stop_stream()
-    stream.close()
 
 def piano_key_nr_to_string(piano_key_nr: int) -> str:
     note_strings = {
@@ -198,29 +109,6 @@ root_config_new = {
     "description": "Main Menu",
     "activation_key": "l",
     "children": [
-        {
-            "activation_key": "a",
-            "description": "Play Loop / Oneshot",
-            "activate": play_activate,
-            "deactivate": play_deactivate,
-            "children": [
-                {
-                    "activation_key": "o",
-                    "description": "Make sound once",
-                    "action": play_once
-                },
-                {
-                    "activation_key": "c",
-                    "description": "Make sound continuously",
-                    "action": play_continuously
-                },
-                {
-                    "activation_key": "s",
-                    "description": "Stop the sound",
-                    "action": stop_playing
-                }
-            ]
-        },
         {
             "activation_key": "0",
             "description": "Play Piano Roll",
